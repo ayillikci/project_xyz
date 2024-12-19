595544f8002531c8a6cf9d2aa108b8d863dacba6
ayillikci <alperenyillikci@gmail.com>
2024-12-19 19:48:35 +0100

    .replace for undefined

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 //const BASE_URL = "http://localhost:8000";
 //const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost/api"
 //const BASE_URL = "https://project-xyz.onrender.com"
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL.replace(/\/+$/, ""); // Remove trailing slash if present
 
 console.log('BASE_URL:', process.env.REACT_APP_BASE_URL);
 
